80695a5be41eed31c74543ef5da8654749d06400
baller70 <135007227+baller70@users.noreply.github.com>
2025-06-19 18:33:45 -0400

    feat: Complete commenting system with InlineCommentMarker and tests

--- src/features/comments/index.ts
+++ src/features/comments/index.ts
@@ -1,11 +1,33 @@
-// Types
-export * from './types';
+// Components
+export { CommentList } from './components/CommentList';
+export { CommentItem } from './components/CommentItem';
+export { CommentEditor } from './components/CommentEditor';
+export { InlineCommentMarker } from './components/InlineCommentMarker';
+export { CommentSection } from './components/CommentSection';
+export { CommentForm } from './components/CommentForm';
 
 // Hooks
 export { useComments } from './hooks/useComments';
+export { useMentionSuggestions } from './hooks/useMentionSuggestions';
+export { useCommentNotifications } from './hooks/useCommentNotifications';
 
-// Components
-export { CommentSection } from './components/CommentSection';
-export { CommentThread } from './components/CommentThread';
-export { CommentItem } from './components/CommentItem';
-export { CommentForm } from './components/CommentForm'; 
+// Services
+export { CommentsService } from './services/CommentsService';
+export { MentionsService } from './services/MentionsService';
+
+// Types
+export type {
+  Comment,
+  CommentThread,
+  CommentStats,
+  CommentPermissions,
+  CreateCommentInput,
+  UpdateCommentInput,
+  CommentFilters,
+  CommentsResponse,
+  MentionSuggestion,
+  CommentNotification,
+  CommentEvent,
+  User,
+  CommentAttachment
+} from './types'; 
